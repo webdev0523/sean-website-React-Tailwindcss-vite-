5847adcc0182198d92416862f8a3b3b2d3121b8a
GolangMan0523 <Senior0523@proton.me>
2024-03-19 22:44:23 -0700

    -

--- src/sections/evlanding/paradise/marquee.jsx
+++ src/sections/evlanding/paradise/marquee.jsx
@@ -5,14 +5,14 @@ export default function Marquee() {
         <>
             <div className="relative pb-1 origin-bottom -rotate-[2.54deg] scrolling-text-container">
                 <div className="h-[70px] w-full flex items-center scrolling-text-left">
-                    <p behavior="scroll" direction="right" className="text-black font-['Neuething'] text-2xl font-extrabold drop-shadow-[2px_-2px_0px_rgba(255,255,255)] whitespace-pre ">TESLA Y   |   GIVEAWAY   |   ISUZU DMAX   |   GIVEAWAY   |   50000$ CASH   |   GIVEAWAY   |   ISUZU DMAX   |   GIVEAWAY</p>
+                    <p behavior="scroll" direction="right" className="text-black font-['Neuething'] text-2xl font-extrabold drop-shadow-[2px_-2px_0px_rgba(255,255,255)] whitespace-pre ">TESLA Y   |   GIVEAWAY   |   ISUZU DMAX   |   GIVEAWAY   |   $50000 CASH   |   GIVEAWAY   |   ISUZU DMAX   |   GIVEAWAY</p>
                 </div>
                 <div className="bg-[#56DFE6] absolute h-[70px] w-full left-0 right-0 top-0 -z-10" />
             </div>
 
             <div className="relative origin-bottom -rotate-[2.54deg] scrolling-text-container">
                 <div className="h-[70px] w-full flex items-center scrolling-text-right">
-                    <p behavior="scroll" direction="left" className="text-white font-['Neuething'] text-2xl font-extrabold drop-shadow-[1px_-1px_0px_rgba(255,255,255)] whitespace-pre ">TESLA Y   |   GIVEAWAY   |   ISUZU DMAX   |   GIVEAWAY   |   50000$ CASH   |   GIVEAWAY   |   ISUZU DMAX   |   GIVEAWAY </p>
+                    <p behavior="scroll" direction="left" className="text-white font-['Neuething'] text-2xl font-extrabold drop-shadow-[1px_-1px_0px_rgba(255,255,255)] whitespace-pre ">TESLA Y   |   GIVEAWAY   |   ISUZU DMAX   |   GIVEAWAY   |   $50000 CASH   |   GIVEAWAY   |   ISUZU DMAX   |   GIVEAWAY </p>
                 </div>
                 <div className="bg-[#FF4C00] absolute h-[70px] w-full left-0 right-0 top-0 -z-10" ></div>
             </div>
