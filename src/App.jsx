712a663640914da7e2b88755a610c12dd1c5fac7
GolangMan0523 <Senior0523@proton.me>
2024-03-19 23:51:06 -0700

    -

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,8 @@ function App() {
 
   return (
     <Layout>
-      {/* <GiveawayPage /> */}
-      <EVLandingPage />  
+      <GiveawayPage />
+      {/* <EVLandingPage />   */}
     </Layout>
   );
 }
