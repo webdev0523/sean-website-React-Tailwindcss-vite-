90d2d0b477dfd899c2017f7b728d50886d4842ab
GolangMan0523 <Senior0523@proton.me>
2024-03-24 02:50:41 -0700

    -

--- src/sections/evlanding/paradise/marquee.jsx
+++ src/sections/evlanding/paradise/marquee.jsx
@@ -12,7 +12,7 @@ export default function Marquee() {
 
             <div className="relative origin-bottom -rotate-[2.54deg] scrolling-text-container">
                 <div className="md:h-[70px] h-[40px] w-full flex items-center scrolling-text-right">
-                    <p behavior="scroll" direction="left" className="text-white font-['Neuething'] text-lg md:text-2xl font-extrabold drop-shadow-[1px_-1px_0px_rgba(255,255,255)] whitespace-pre ">TESLA Y   |   GIVEAWAY   |   ISUZU DMAX   |   GIVEAWAY   |   $50000 CASH   |   GIVEAWAY   |   ISUZU DMAX   |   GIVEAWAY </p>
+                    <p behavior="scroll" direction="left" className="text-white font-['Neuething'] text-lg md:text-2xl font-extrabold drop-shadow-[1px_-1px_0px_rgba(0,0,0)] whitespace-pre ">TESLA Y   |   GIVEAWAY   |   ISUZU DMAX   |   GIVEAWAY   |   $50000 CASH   |   GIVEAWAY   |   ISUZU DMAX   |   GIVEAWAY </p>
                 </div>
                 <div className="bg-[#FF4C00] absolute md:h-[70px] h-[40px] w-full left-0 right-0 top-0 -z-10" ></div>
             </div>
